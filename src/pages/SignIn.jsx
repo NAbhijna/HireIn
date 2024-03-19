576f39c4fb9c1063922ce5e63b129a6ce8dd167e
NAbhijna <abhijnanalka@gmail.com>
2024-03-19 20:01:33 +0530

    First

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,87 +1,9 @@
-import React from 'react';
-import {FaEyeSlash,FaEye } from "react-icons/fa"
-import { useState } from "react";
-import OAuth from "../components/OAuth";
-import { Link, Navigate, useNavigate } from "react-router-dom";
-import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
-import { toast } from "react-toastify";
-
+import React from 'react'
 
 export default function SignIn() {
-  const [showPassword, setShowPassword]=useState(false);
- const [formData, setFormData]=useState({
-    email: "",
-    password: "",
-  });
-  const {email, password}= formData;
-  const navigate = useNavigate();
-  function onChange(e){
-    setFormData((prevState)=>({
-      ...prevState,
-      [e.target.id]:e.target.value,
-    }));
-  }
-  async function onSubmit(e)
-  {
-    e.preventDefault();
-    try{
-      const auth = getAuth();
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      if(userCredential.user)
-      {
-        navigate("/")
-      }
-    }catch(error)
-    {
-      toast.error("Bad user credentials");
-    }
-  }
   return (
-    <section>
-      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
-    <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
-      <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
-        <img src="https://media.istockphoto.com/id/494347130/photo/friends-having-dinner.webp?b=1&s=170667a&w=0&k=20&c=w1YQQ0xFgIyzNHnp0_zuprqB_YtPmAlSub8f8BousLk="alt="Share Food" 
-        className="w-full rounded-2xl"/>
-      </div>
-      <div className="w-full md:w-[67%] lg:w[40%] lg:ml-20">
-        <form onSubmit={onSubmit}>
-          <input type="email" id="email" value={email} onChange={onChange}
-          placeholder="Email address"
-          className="mb-6 w-full px-4 py-2 text-x1
-          text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/>
-          <div className="relative mb-6">
-          <input
-          type={showPassword ? "text" : "password"} id="password" 
-          value={password} onChange={onChange}
-          placeholder="Password"className="w-full px-4 py-2 text-x1
-          text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"/>
-          {showPassword ?  (<FaEyeSlash className="absolute right-3 top-3 text-x1 cursor-pointer"
-          onClick={()=>setShowPassword
-          ((prevState)=>!prevState)}/>):(<FaEye className="absolute right-3 top-3 text-x1 cursor-pointer"onClick={()=>setShowPassword
-            ((prevState)=>!prevState)}/>)}
-          </div>
-          <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
-            <p className="mb-6">Don't have an account?
-            <Link to="/sign-up" className="text-red-800 hover:text-red-900 transition duration-200 ease-in-out ml-1">Register
-            </Link>
-            </p>
-            <p>
-              <Link to="/forgot-password"className="text-blue-700 hover:text-blue-800
-               transition duration-200 ease-in-out ml-1">Forgot password?</Link>
-            </p>
-          </div>
-          <button className="w-full bg-blue-800 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-900 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-950"
-        type="submit">Sign In</button>
-       <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-red-900 after:border-t after:flex-1 after:border-red-900">
-              <p className="text-center font-semibold mx-4">OR</p>
-            </div>
-            <OAuth />
-        </form>
-        
-      </div>
+    <div>
+      
     </div>
-    </section>
   )
 }
-
